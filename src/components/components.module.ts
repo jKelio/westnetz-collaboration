a1cee68ac5cf1dd7684ff7733632c4d2290264b7
oskarherz <herz.oskar@gmail.com>
2018-03-17 21:45:06 +0100

    no message

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -4,13 +4,20 @@ import { ScheduleComponent } from './schedule/schedule';
 import { AllProcessesComponent } from './all-processes/all-processes';
 import { MapComponent } from './map/map';
 import { DocHistoryComponent } from './doc-history/doc-history';
+
+import { AgmCoreModule } from '@agm/core';
+
 @NgModule({
 	declarations: [ActiveProcessComponent,
     ScheduleComponent,
     AllProcessesComponent,
     MapComponent,
     DocHistoryComponent],
-	imports: [],
+	imports: [
+        AgmCoreModule.forRoot({
+            apiKey: 'AIzaSyCpNqmrx55YZFF3lwPioDSnbXmGMI_8LLU'
+        })
+    ],
 	exports: [ActiveProcessComponent,
     ScheduleComponent,
     AllProcessesComponent,
--- src/components/map/map.ts
+++ src/components/map/map.ts
@@ -1,20 +1,13 @@
 import { Component } from '@angular/core';
-import * as GoogleMapsLoader from 'google-maps';
+import { AgmCoreModule } from '@agm/core';
 
 @Component({
   selector: 'map',
   templateUrl: 'map.html'
 })
 export class MapComponent {
-
-  constructor() {
-    GoogleMapsLoader.KEY = 'AIzaSyCpNqmrx55YZFF3lwPioDSnbXmGMI_8LLU';
-    GoogleMapsLoader.load(function(google) {
-      var test = new google.maps.Map('map_canvas', {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-      });
-    });
-  }
+  latitude: number;
+  longitude: number;
+  constructor() {}
 
 }
